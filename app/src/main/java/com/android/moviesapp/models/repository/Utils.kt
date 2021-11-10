03069994d08bcc06991dabcb2be0dac84f9084c7
Moataz Mostafa Ibrahim <moataz.mostafa95@gmail.com>
2021-11-10 06:28:25 +0200

    view large poster

--- app/src/main/java/com/android/moviesapp/models/repository/Utils.kt
+++ app/src/main/java/com/android/moviesapp/models/repository/Utils.kt
@@ -3,9 +3,9 @@ package com.android.moviesapp.models.repository
 object Utils {
     const val BASE_URL ="https://api.themoviedb.org/"
     const val IMAGE_BASE_URL ="https://image.tmdb.org/t/p/w500"
+    const val IMAGE_ORIGINAL_BASE_URL ="https://image.tmdb.org/t/p/original"
     const val MOVIE_URL ="3/movie"
     const val SEARCH_URL ="3/search/movie"
-    const val YOUTUBE_BASE_URL ="https://www.youtube.com/watch"
     const val API_KEY ="1280a36dda2e573fd1f0fb873e40c0db"
     const val NOW_PLAYING = 1
     const val UPCOMING = 2
--- app/src/main/java/com/android/moviesapp/view/MovieActivity.kt
+++ app/src/main/java/com/android/moviesapp/view/MovieActivity.kt
@@ -1,18 +1,27 @@
 package com.android.moviesapp.view
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import com.android.moviesapp.R
 import com.android.moviesapp.models.Movies
 import com.android.moviesapp.models.repository.Utils
 import com.android.moviesapp.models.repository.Utils.GOOGLE_API
+import com.android.moviesapp.models.repository.Utils.IMAGE_BASE_URL
+import com.android.moviesapp.models.repository.Utils.IMAGE_ORIGINAL_BASE_URL
 import com.android.moviesapp.viewmodels.MovieViewModel
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_movie.*
+import kotlinx.android.synthetic.main.dialog_poster_image.*
 
 class MovieActivity : YouTubeBaseActivity() {
     lateinit var movieViewModel: MovieViewModel
@@ -28,8 +37,9 @@ class MovieActivity : YouTubeBaseActivity() {
         movie_release_dateTV.text = "${movie_release_dateTV.text} ${movieViewModel.movie.releaseDate}"
         movie_rateTV.text = "${movie_rateTV.text} ${movieViewModel.movie.voteAverage}"
         movie_overviewTV.text = "${movie_overviewTV.text} ${movieViewModel.movie.overview}"
-        Picasso.get().load("${Utils.IMAGE_BASE_URL}${movieViewModel.movie.backdropPath}").into(movie_main_imageIV)
-        Picasso.get().load("${Utils.IMAGE_BASE_URL}${movieViewModel.movie.posterPath}").into(movie_poster_imageIV)
+        Picasso.get().load("${IMAGE_BASE_URL}${movieViewModel.movie.backdropPath}").into(movie_main_imageIV)
+        Picasso.get().load("${IMAGE_BASE_URL}${movieViewModel.movie.posterPath}").into(movie_poster_imageIV)
+        movie_poster_imageIV.setOnClickListener { showPosterDialog() }
         movie_play_buttonIV.setOnClickListener {
             if(onInitializedListener!=null) {
                 movie_youTubePlayerView.initialize(GOOGLE_API, onInitializedListener)
@@ -59,5 +69,18 @@ class MovieActivity : YouTubeBaseActivity() {
                 }
             }
         }
+    }
+    private fun showPosterDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_poster_image)
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setGravity(Gravity.CENTER)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        Picasso.get().load("${IMAGE_ORIGINAL_BASE_URL}${movieViewModel.movie.posterPath}").into(dialog.dialog_posterIV)
+        dialog.show()
     }
 }
